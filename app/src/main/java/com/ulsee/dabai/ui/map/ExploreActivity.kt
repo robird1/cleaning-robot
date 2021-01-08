a92c1fab1fc0337a17cac2304fbe8c210f43e888
codus <codus.hsu@ulsee.com>
2021-01-08 16:36:15 +0800

    完成 UI搖桿 整合移動功能。另外移除連接按鈕

--- app/src/main/java/com/ulsee/dabai/ui/map/ExploreActivity.kt
+++ app/src/main/java/com/ulsee/dabai/ui/map/ExploreActivity.kt
@@ -13,6 +13,9 @@ import androidx.lifecycle.ViewModelProvider
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.ulsee.dabai.R
+import com.ulsee.dabai.data.RobotRepository
+import io.github.controlwear.virtual.joystick.android.JoystickView
+import java.lang.Exception
 
 private val TAG = ExploreActivity::class.java.simpleName
 
@@ -36,6 +39,7 @@ class ExploreActivity: AppCompatActivity() {
 
     private lateinit var mProgressDialog: ProgressDialog
     private lateinit var viewModel: ExploreViewModel
+    private lateinit var robotRepository: RobotRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +48,7 @@ class ExploreActivity: AppCompatActivity() {
         ButterKnife.bind(this)
         imageView = findViewById(R.id.imageView)
         progressBar = findViewById(R.id.progressBar)
+        initRobotRepository()
         initViewModel()
         initExitCreateMapObserver()
         mProgressDialog = ProgressDialog(this)
@@ -58,6 +63,31 @@ class ExploreActivity: AppCompatActivity() {
         viewModel.exitCreateMapResult.observe(this, Observer {
             progressBar.visibility = View.INVISIBLE
             finish()
+        })
+    }
+
+    private fun initRobotRepository() {
+        robotRepository = RobotRepository()
+
+        val ctx = this
+        robotRepository.setEventListener(object: RobotRepository.EventListener{
+            override fun onPositionUpdated(pose: Array<Double>) {
+                // TODO
+            }
+
+            override fun onConnected() {
+                runOnUiThread { Toast.makeText(ctx, "connected", Toast.LENGTH_SHORT).show() }
+            }
+
+            override fun onConnectFailed(exception: Exception) {
+                runOnUiThread { Toast.makeText(ctx, "connect failed: "+exception.message, Toast.LENGTH_LONG).show() }
+                finish()
+            }
+
+            override fun onError(exception: Exception) {
+                runOnUiThread { Toast.makeText(ctx, "error: "+exception.message, Toast.LENGTH_LONG).show() }
+                finish()
+            }
         })
     }
 
@@ -94,65 +124,54 @@ class ExploreActivity: AppCompatActivity() {
 
 
     private fun initJoyStick() {
-//        val isStop = booleanArrayOf(false)
-//        val movement = doubleArrayOf(0.0)
-//        val rotation = doubleArrayOf(0.0)
-//        (findViewById<View>(R.id.joystick) as JoystickView).setOnMoveListener { intAngle: Int, intStrength: Int ->
-////            Log.d(TAG, "[Enter] onMoveListener() intAngle: "+ intAngle+ "intStrength: "+intStrength);
-//            joystickOnMove!!.text = "angle: $intAngle strength: $intStrength"
-//
-//            // TODO
-////            if (!mRobotClient_sendCommand.isConnected()) {
-////                debugView!!.text = "!mRobotClient_sendCommand.isConnected() in onMoveListener()"
-////                return@setOnMoveListener
-////            }
-//            val angle = intAngle.toDouble()
-//            val strength = intStrength.toDouble()
-//            rotation[0] = 0
-//            movement[0] = strength / 100.0
-//            isStop[0] = intAngle == 0 && intStrength == 0
-//            //            isStop[0] = intStrength == 0;
-//            if (isStop[0]) {
-//                stopMove()
-//                return@setOnMoveListener
-//            }
-//            // calculate movement forward & backward
-//            /* 0~ 1*/if (angle > 0 && angle <= 90) movement[0] = movement[0] * angle / 90.0
-//            /* 1~ 0*/if (angle > 90 && angle <= 180) movement[0] =
-//            movement[0] * (1.0 - (angle - 90.0) / 90.0)
-//            /* 0~-1*/if (angle > 180 && angle <= 270) movement[0] =
-//            movement[0] * (-(angle - 180.0) / 90.0)
-//            /*-1~ 0*/if (angle > 270 && angle < 360) movement[0] =
-//            movement[0] * (-1 + (angle - 270.0) / 90.0)
-//            // calculate rotate
-//            /*-1~ 0*/if (angle > 0 && angle <= 90) rotation[0] = -(90.0 - angle) / 90.0
-//            /* 0~ 1*/if (angle > 90 && angle <= 180) rotation[0] = (angle - 90.0) / 90.0
-//            /* 1~ 0*/if (angle > 180 && angle <= 270) rotation[0] = 1 - (angle - 180.0) / 90.0
-//            /* 0~-1*/if (angle > 270 && angle < 360) rotation[0] = -(angle - 270.0) / 90
-//            // 靠近上下時不轉彎，靠近左右時不前後移動
-//            if (angle > 70 && angle < 110 || angle > 250 && angle < 290) rotation[0] = 0
-//            if (angle > 340 || angle < 20 || angle > 160 && angle < 200) movement[0] = 0
-//            // 下方，靠近正左正右時，轉彎方向不變
-////            if (angle > 340 || angle > 180 && angle < 200) rotation[0] = -rotation[0];
-//
-//
-////            Log.d(TAG, String.format("angle=%f, strength=%f, rotation[0]=%f, movement[0]=%f", angle, strength, rotation[0], movement[0]));
-//            if (isStop[0]) return@setOnMoveListener
-//
-//            // TODO
-////            movement[0] *= getSpeedRatio()
-////            mRobotClient_sendCommand.requestMove(movement[0], rotation[0])
-////                .subscribe({ Void -> }) { t ->
-////                    t.printStackTrace()
-////                    Log.d(
-////                        ExploreActivity.TAG,
-////                        "[Failed] mRobotClient_sendCommand.requestMove()"
-////                    )
-////                }
-//        }
+        val isStop = booleanArrayOf(false)
+        val movement = doubleArrayOf(0.0)
+        val rotation = doubleArrayOf(0.0)
+        (findViewById<View>(R.id.joystick) as JoystickView).setOnMoveListener { intAngle: Int, intStrength: Int ->
+//            Log.d(TAG, "[Enter] onMoveListener() intAngle: "+ intAngle+ "intStrength: "+intStrength);
+            //joystickOnMove!!.text = "angle: $intAngle strength: $intStrength"
+
+            val angle = intAngle.toDouble()
+            val strength = intStrength.toDouble()
+            rotation[0] = 0.0
+            movement[0] = strength / 100.0
+            isStop[0] = intAngle == 0 && intStrength == 0
+            //            isStop[0] = intStrength == 0;
+            if (isStop[0]) {
+                stopMove()
+                return@setOnMoveListener
+            }
+            // calculate movement forward & backward
+            /* 0~ 1*/if (angle > 0 && angle <= 90) movement[0] = movement[0] * angle / 90.0
+            /* 1~ 0*/if (angle > 90 && angle <= 180) movement[0] =
+            movement[0] * (1.0 - (angle - 90.0) / 90.0)
+            /* 0~-1*/if (angle > 180 && angle <= 270) movement[0] =
+            movement[0] * (-(angle - 180.0) / 90.0)
+            /*-1~ 0*/if (angle > 270 && angle < 360) movement[0] =
+            movement[0] * (-1 + (angle - 270.0) / 90.0)
+            // calculate rotate
+            /*-1~ 0*/if (angle > 0 && angle <= 90) rotation[0] = -(90.0 - angle) / 90.0
+            /* 0~ 1*/if (angle > 90 && angle <= 180) rotation[0] = (angle - 90.0) / 90.0
+            /* 1~ 0*/if (angle > 180 && angle <= 270) rotation[0] = 1 - (angle - 180.0) / 90.0
+            /* 0~-1*/if (angle > 270 && angle < 360) rotation[0] = -(angle - 270.0) / 90
+            // 靠近上下時不轉彎，靠近左右時不前後移動
+            if (angle > 70 && angle < 110 || angle > 250 && angle < 290) rotation[0] = 0.0
+            if (angle > 340 || angle < 20 || angle > 160 && angle < 200) movement[0] = 0.0
+            // 下方，靠近正左正右時，轉彎方向不變
+//            if (angle > 340 || angle > 180 && angle < 200) rotation[0] = -rotation[0];
+
+            if (isStop[0]) return@setOnMoveListener
+
+            if (movement[0] != 0.0) {
+                robotRepository.move(movement[0])
+            }
+            if (rotation[0] != 0.0) {
+                robotRepository.rotate(rotation[0]* 50)
+            }
+        }
     }
 
     private fun stopMove() {
-        // TODO
+        robotRepository.move(0.0)
     }
 }
